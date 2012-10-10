71ce721270a81fad6fd38fb69fe1a610ce17a0e1
Christophe Coenraets <ccoenraets@gmail.com>
2012-10-10 11:19:39 -0400

    re-enabled crud

--- routes/wines.js
+++ routes/wines.js
@@ -43,54 +43,51 @@ exports.findAll = function(req, res) {
 
 exports.addWine = function(req, res) {
     var wine = req.body;
-//    console.log('Adding wine: ' + JSON.stringify(wine));
-//    db.collection('wines', function(err, collection) {
-//        collection.insert(wine, {safe:true}, function(err, result) {
-//            if (err) {
-//                res.send({'error':'An error has occurred'});
-//            } else {
-//                console.log('Success: ' + JSON.stringify(result[0]));
-//                res.send(result[0]);
-//            }
-//        });
-//    });
-    res.send(wine);
+    console.log('Adding wine: ' + JSON.stringify(wine));
+    db.collection('wines', function(err, collection) {
+        collection.insert(wine, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('Success: ' + JSON.stringify(result[0]));
+                res.send(result[0]);
+            }
+        });
+    });
 }
 
 exports.updateWine = function(req, res) {
     var id = req.params.id;
     var wine = req.body;
-//    delete wine._id;
-//    console.log('Updating wine: ' + id);
-//    console.log(JSON.stringify(wine));
-//    db.collection('wines', function(err, collection) {
-//        collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
-//            if (err) {
-//                console.log('Error updating wine: ' + err);
-//                res.send({'error':'An error has occurred'});
-//            } else {
-//                console.log('' + result + ' document(s) updated');
-//                res.send(wine);
-//            }
-//        });
-//    });
-    res.send(wine);
+    delete wine._id;
+    console.log('Updating wine: ' + id);
+    console.log(JSON.stringify(wine));
+    db.collection('wines', function(err, collection) {
+        collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
+            if (err) {
+                console.log('Error updating wine: ' + err);
+                res.send({'error':'An error has occurred'});
+            } else {
+                console.log('' + result + ' document(s) updated');
+                res.send(wine);
+            }
+        });
+    });
 }
 
 exports.deleteWine = function(req, res) {
-//    var id = req.params.id;
-//    console.log('Deleting wine: ' + id);
-//    db.collection('wines', function(err, collection) {
-//        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-//            if (err) {
-//                res.send({'error':'An error has occurred - ' + err});
-//            } else {
-//                console.log('' + result + ' document(s) deleted');
-//                res.send(req.body);
-//            }
-//        });
-//    });
-      res.send(req.body);
+    var id = req.params.id;
+    console.log('Deleting wine: ' + id);
+    db.collection('wines', function(err, collection) {
+        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+            if (err) {
+                res.send({'error':'An error has occurred - ' + err});
+            } else {
+                console.log('' + result + ' document(s) deleted');
+                res.send(req.body);
+            }
+        });
+    });
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
