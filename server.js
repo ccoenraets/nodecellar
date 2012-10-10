597659b14afe4e37aa512380d56d5a3751f5a8fb
Christophe Coenraets <ccoenraets@gmail.com>
2012-10-10 12:19:13 -0400

    format

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser()),
     app.use(express.static(path.join(__dirname, 'public')));
 });
